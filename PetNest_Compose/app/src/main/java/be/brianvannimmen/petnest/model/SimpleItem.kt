b7fa00d48804301ca504967635bfff449a6b5568
BrianVanNimmen <brianvannimmen@gmail.com>
2026-08-07 20:40:46 +0200

    Revert "PetNest Complete"
    This reverts commit 80b52a97fdb844db72a6399ae8836bdc622abf91.

--- PetNest_Compose/app/src/main/java/be/brianvannimmen/petnest/model/SimpleItem.kt
+++
@@ -1,6 +0,0 @@
-package be.brianvannimmen.petnest.model
-
-data class SimpleItem(
-    val id: Int,
-    val naam: String
-)
--- PetNest_Compose/app/src/main/java/be/brianvannimmen/petnest/ui/state/HuisdierenUiState.kt
+++
@@ -1,9 +0,0 @@
-package be.brianvannimmen.petnest.ui.state
-
-import be.brianvannimmen.petnest.network.Huisdier
-
-data class HuisdierenUiState(
-    val dieren: List<Huisdier> = emptyList(),
-    val isLoading: Boolean = false,
-    val error: String? = null
-)
